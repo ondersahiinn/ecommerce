7d8e603184d1be4a4f311977691114be319c777b
Oguzhan Celik <ogzzcelik@gmail.com>
2022-06-11 02:38:18 +0300

    Login & Register tabs button added

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, Tabs } from 'antd';
 
-export const Login:React.FC = () => {
+const { TabPane } = Tabs;
+export const Login: React.FC = () => {
 
     const onFinish = (values: any) => {
         console.log('Success:', values);
@@ -11,45 +12,51 @@ export const Login:React.FC = () => {
         console.log('Failed:', errorInfo);
     };
 
+    const onChange = (key: string) => {
+        console.log(key);
+    };
+
     return (
-        <Form
-            name="basic"
-            wrapperCol={{ span: 6}}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            
-        >
-            <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-                
+            <Tabs defaultActiveKey="1" type='line'  onChange={onChange}  >
+                <TabPane tab="Giriş Yap" key="1"  >
+                <Form
+                name="basic"
+                wrapperCol={{ span: 6 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Input placeholder="Username" className='p-3 rounded-lg text-md' />
-            </Form.Item>
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input placeholder="Username" className='p-3 rounded-lg text-md bg-[#eeeeee]' />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password placeholder="Password" className='login-input-password p-3 rounded-lg text-md bg-[#eeeeee]' />
+                </Form.Item>
 
-            <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Input.Password placeholder="Password" className='p-3 rounded-lg text-md'  />
-            </Form.Item>
-
-            <Form.Item name="remember" valuePropName="checked" >
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item
+                <Form.Item name="remember" valuePropName="checked" >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <Form.Item
                 // name="Login_Button"
                 // rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Button htmlType="submit"  className='bg-primary-base text-white w-full flex justify-center  p-6  rounded-lg  text-md items-center'>
-                    Giriş Yap
-                </Button>
-            </Form.Item>
-           
-        </Form>
+                >
+                    <Button htmlType="submit" className='bg-primary-base text-white w-full flex justify-center  p-6  rounded-lg  text-md items-center'>
+                        Giriş Yap
+                    </Button>
+                </Form.Item>
+            </Form>
+                </TabPane>
+                <TabPane tab="Kayıt Ol" key="2">
+                    Content of Tab Pane 2
+                </TabPane>
+            </Tabs>
     )
 }
 
