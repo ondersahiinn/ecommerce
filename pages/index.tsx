f7abefbc663c1ec92f0fb9478f9f6d6f774b8500
Oguzhan Celik <ogzzcelik@gmail.com>
2022-06-10 23:21:15 +0300

    style: :bug: Düzenlendi
    Daha iyi açıklama

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Container, Header, Main, Footer, Cards } from "@components";
 const Home: React.FC = () => {
   return (
     <Container>
+      
       <Header />
       <Main />
       <Cards />
