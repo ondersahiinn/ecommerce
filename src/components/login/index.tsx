3009da1980ced6983da72f2924ac6ced553f2c0b
Oguzhan Celik <ogzzcelik@gmail.com>
2022-06-15 01:25:15 +0300

    Login & register being developed

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -17,46 +17,72 @@ export const Login: React.FC = () => {
     };
 
     return (
-            <Tabs defaultActiveKey="1" type='line'  onChange={onChange}  >
-                <TabPane tab="Giriş Yap" key="1"  >
+        <Tabs defaultActiveKey="1" type='line' onChange={onChange} className='flex'  >
+            <TabPane tab="Giriş Yap" key="1"  >
                 <Form
-                name="basic"
-                wrapperCol={{ span: 6 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+                    name="basic"
+                    wrapperCol={{ span: 6 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Input placeholder="Username" className='p-3 rounded-lg text-md bg-[#eeeeee]' />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password placeholder="Password" className='login-input-password p-3 rounded-lg text-md bg-[#eeeeee]' />
-                </Form.Item>
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input placeholder="Username" className='p-3 rounded-lg text-md bg-[#eeeeee]' />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password placeholder="Password" className='login-input-password p-3 rounded-lg text-md bg-[#eeeeee]' />
+                    </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked" >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-                <Form.Item
-                // name="Login_Button"
-                // rules={[{ required: true, message: 'Please input your password!' }]}
+                    <Form.Item name="remember" valuePropName="checked" >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                    <Form.Item
+                    // name="Login_Button"
+                    // rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Button htmlType="submit" className='bg-primary-base text-white w-full flex justify-center  p-6  rounded-lg  text-md items-center'>
+                            Giriş Yap
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </TabPane>
+            <TabPane tab="Kayıt Ol" key="2">
+                <Form
+                    name="basic"
+                    wrapperCol={{ span: 6 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Button htmlType="submit" className='bg-primary-base text-white w-full flex justify-center  p-6  rounded-lg  text-md items-center'>
-                        Giriş Yap
-                    </Button>
-                </Form.Item>
-            </Form>
-                </TabPane>
-                <TabPane tab="Kayıt Ol" key="2">
-                    Content of Tab Pane 2
-                </TabPane>
-            </Tabs>
+                    <div className='max-w-3 mb-5 '>
+                    <h2>E-posta adresi ile üye ol</h2>
+                    </div>
+                    <Form.Item
+                        name="Email"
+                        rules={[{ required: true, message: 'E-posta adresinizi girmelisiniz.' }]}
+                    >
+                        <Input placeholder="E-posta adresi" className='p-3 rounded-lg text-md bg-[#eeeeee]' />
+                    </Form.Item>            
+                    <Form.Item
+                    // name="Login_Button"
+                    // rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Button htmlType="submit" className='bg-primary-base text-white w-full flex justify-center  p-6  rounded-lg  text-md items-center'>
+                            Devam Et
+                        </Button>
+                    </Form.Item>
+                    <p className='text-[11px] text-[#646464] max-w-xs text-left'>Kişisel verileriniz, <strong role="presentation"> Aydınlatma Metni </strong>kapsamında işlenmektedir. “Üye ol” veya “Sosyal Hesap” butonlarından birine basarak <strong role="presentation"> Üyelik Sözleşmesi</strong>’ni, <strong role="presentation"> Rıza Metni</strong>’ni, Çerez Politikası’nı okuduğunuzu ve kabul ettiğinizi onaylıyorsunuz.</p>
+                </Form>
+            </TabPane>
+        </Tabs>
     )
 }
 
