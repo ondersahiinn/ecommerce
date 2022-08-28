6135aae437cab36dae9dc9f06c8c3afe37689f70
Önder Şahin <ondershin@gmail.com>
2022-08-28 22:12:52 +0300

    mongoose connection readyState control

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 const connectDB = (handler: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
-  if (mongoose.connections[0].readyState) {
+  if (mongoose.connection.readyState) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.MONGODB_URI as string);
-  return handler(req, res);
+  else {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    return handler(req, res);
+  }
+
 };
 
 
