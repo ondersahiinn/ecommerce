fc91621d30213fc458aff51958ea59fc63e11285
berkinoktay <berkinoktayy@gmail.com>
2022-12-24 22:06:39 +0300

    edit(product-card):add new types

--- interfaces/productCard.ts
+++ interfaces/productCard.ts
@@ -2,9 +2,10 @@ export interface ProductItemProps {
   type?: "default" | "small";
   id: string | number;
   name: string;
-  image: string;
+  images: string[];
   price: number;
   brand: string;
   discount_percentage: number;
   disabled: boolean;
+  showSlider?: boolean;
 }
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -13,6 +13,12 @@ import HButton from '@components/HButton';
 import { Image } from 'antd';
 
 export const Main: React.FC = () => {
+  const images = [
+    "https://productimages.hepsiburada.net/s/315/550/110000308343964.jpg",
+    "https://productimages.hepsiburada.net/s/168/550/110000131103936.jpg",
+    "https://productimages.hepsiburada.net/s/168/550/110000131103937.jpg",
+    "https://productimages.hepsiburada.net/s/168/550/110000131103938.jpg"
+  ]
   const dispatch = useDispatch();
   const [theme, setTheme] = React.useState(
     typeof window !== 'undefined' ? localStorage.getItem('site-theme') : 'light'
@@ -32,15 +38,15 @@ export const Main: React.FC = () => {
 
 
       <div className='max-w-7xl mx-auto my-12 px-3 grid grid-cols-4 gap-4 items-start '>
-        <ProductItem type='small' brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem type='small' brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
-        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} image="https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg" price={22222} />
+        <ProductItem type='small' brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={images} price={22222} />
+        <ProductItem type='small' brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
+        <ProductItem brand='Berkin' disabled={false} id={1} name="lorem ipsum" discount_percentage={24} images={["https://productimages.hepsiburada.net/s/189/1100/110000155170656.jpg"]} price={22222} />
 
       </div>
 
